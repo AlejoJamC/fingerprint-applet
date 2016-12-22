a35fccf16ec61c10215dfff9459bcf52bae3834a
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-12-22 05:53:57 -0500

    update java applet

--- src/EnrollFingerprint/Enrollment.java
+++ src/EnrollFingerprint/Enrollment.java
@@ -5,22 +5,226 @@
  */
 package EnrollFingerprint;
 
+import com.digitalpersona.onetouch.*;
+import com.digitalpersona.onetouch.capture.*;
+import com.digitalpersona.onetouch.capture.event.*;
+import com.digitalpersona.onetouch.processing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.*;
 import javax.swing.JApplet;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 /**
- * Size: width=700, height=360
+ *
  * @author AlejoDesktop
  */
 public class Enrollment extends JApplet {
+    
+    public static String TEMPLATE_PROPERTY = "template";
+    private DPFPTemplate template;
+    private DPFPCapture capturer = DPFPGlobal.getCaptureFactory().createCapture();
+    private DPFPEnrollment enroller = DPFPGlobal.getEnrollmentFactory().createEnrollment();
 
-    /**
-     * Creates new form Enroll
-     */
     @Override
-    public void init() {
+    public void init(){
         initComponents();
+        updateStatus();
+        
+       // Event listener actived when fingerprint template is ready
+       this.addPropertyChangeListener(TEMPLATE_PROPERTY, new PropertyChangeListener(){
+           public void propertyChange(PropertyChangeEvent evt){
+               btnSave.setEnabled(template != null);
+               if(evt.getNewValue() == evt.getOldValue()){
+                   return;
+               }
+               if(template != null){
+                   JOptionPane.showMessageDialog(
+                           Enrollment.this,
+                           "La huella capturada esta lista para ser guardada.",
+                           "Captura y Registro de huellas",
+                           JOptionPane.INFORMATION_MESSAGE
+                   );
+               }
+           }
+       });
+        
+        capturer.addDataListener(new DPFPDataAdapter(){
+            @Override public void  dataAcquired(final DPFPDataEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        makeReport("La huella fue capturada.");
+                        setPrompt("Escanear la misma huella de nuevo.");
+                        process(e.getSample());
+                    }
+                });
+            }
+        });
+        
+        capturer.addReaderStatusListener(new DPFPReaderStatusAdapter(){
+            @Override public void  readerConnected(final DPFPReaderStatusEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        makeReport("Lector de huellas conectado.");
+                    }
+                });
+            }
+            @Override public void readerDisconnected(final DPFPReaderStatusEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        makeReport("Lector de huellas desconectado.");
+                    }
+                });
+            }
+        });
+        
+        capturer.addSensorListener(new DPFPSensorAdapter(){
+            @Override public void fingerTouched(final DPFPSensorEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        makeReport("El escaner de huellas fue tocado.");
+                    }
+                });
+            }
+            @Override public void fingerGone(final DPFPSensorEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        makeReport("El dedo fue retirado del escaner de huellas.");
+                    }
+                });
+            }
+        });
+        
+        capturer.addImageQualityListener(new DPFPImageQualityAdapter(){
+            @Override public void onImageQuality(final DPFPImageQualityEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(e.getFeedback().equals(DPFPCaptureFeedback.CAPTURE_FEEDBACK_GOOD)){
+                            makeReport("La calidad de la huella escaneada es buena.");
+                        }else{
+                            makeReport("La calidad de la huella escaneada es pobre.");
+                        }
+                    }
+                });
+            }            
+        });
+    }
+    
+    protected void process(DPFPSample sample){
+        // Show the fingerprint image.
+        drawPicture(convertSampleToBitmap(sample));
+        // Process the fingerprint and create a feature set form the enrollment purpose.
+        DPFPFeatureSet features = extractFeatures(sample, DPFPDataPurpose.DATA_PURPOSE_ENROLLMENT);
+        
+        // Check quality of the fingerprint and add to enroller if it's good.
+        if(features != null) {
+            try {
+                makeReport("Creada la caracterización de la huella.");
+                enroller.addFeatures(features);
+            } catch (DPFPImageQualityException e) {}
+            finally{
+                updateStatus();
+                
+                // Check if a template has been created.
+                switch(enroller.getTemplateStatus()){
+                    case TEMPLATE_STATUS_READY:
+                        stop();
+                        setTemplate(enroller.getTemplate());
+                        setPrompt("Huela lista para verificar.");
+                        btnRead.setEnabled(false);
+                        break;
+                    
+                    case TEMPLATE_STATUS_FAILED:
+                        enroller.clear();
+                        stop();
+                        updateStatus();
+                        setTemplate(null);
+                        JOptionPane.showMessageDialog(Enrollment.this,
+                                "La huella capturada no es validad, repita el registro.",
+                                "Captura y Registro de huellas", JOptionPane.ERROR_MESSAGE
+                        );
+                        start();
+                        break;
+                        
+                }
+            }
+        }
+        
+
+    }
+    
+    public void start(){
+        capturer.startCapture();
+        setPrompt("Scanner de huellas listo a usarse, por favor introduzca la huella.");
+        btnRead.setEnabled(false);
+    }
+    
+    public void stop(){
+        capturer.stopCapture();
+    }
+    
+    public void setStatus(String string){
+        lblStatus.setText(string);
+    }
+    
+    public void setPrompt(String string){
+        txtConsole.setText(string);
+    }
+    
+    public void makeReport(String string){
+        txtLog.append(string + "\n");
+    }
+    
+    public void drawPicture(Image image) {
+        picFingerprint.setIcon(new ImageIcon(
+                image.getScaledInstance(
+                        picFingerprint.getWidth(), 
+                        picFingerprint.getHeight(), 
+                        Image.SCALE_DEFAULT
+                )
+        ));
+    }
+    
+    public DPFPTemplate getTemplate() {
+		return template;
+	}
+	public void setTemplate(DPFPTemplate template) {
+		DPFPTemplate old = this.template;
+		this.template = template;
+		firePropertyChange(TEMPLATE_PROPERTY, old, template);
+	}
+    
+    private void updateStatus(){
+        setStatus(String.format("Toma de huella requiridas: %1$s", enroller.getFeaturesNeeded()));
+    }
+    
+    protected Image convertSampleToBitmap(DPFPSample sample){
+        return DPFPGlobal.getSampleConversionFactory().createImage(sample);
+    }
+    
+    protected DPFPFeatureSet extractFeatures(DPFPSample sample, DPFPDataPurpose purpose){
+        DPFPFeatureExtraction extractor = DPFPGlobal.getFeatureExtractionFactory().createFeatureExtraction();
+        try {
+            return extractor.createFeatureSet(sample, purpose);
+        } catch (DPFPImageQualityException e){
+            return null;
+        }
     }
 
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
@@ -33,11 +237,12 @@ public class Enrollment extends JApplet {
         lblConsole = new javax.swing.JLabel();
         btnSave = new javax.swing.JButton();
         btnCancel = new javax.swing.JButton();
-        jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        lblConsole1 = new javax.swing.JLabel();
+        jPanelLog = new javax.swing.JPanel();
+        JScrollPane1 = new javax.swing.JScrollPane();
+        txtLog = new javax.swing.JTextArea();
+        lblLog = new javax.swing.JLabel();
         btnRead = new javax.swing.JButton();
+        lblStatus = new javax.swing.JLabel();
 
         jPanelBackground.setBackground(new java.awt.Color(238, 241, 245));
 
@@ -80,42 +285,55 @@ public class Enrollment extends JApplet {
         btnSave.setEnabled(false);
 
         btnCancel.setText("Cancelar");
+        btnCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelActionPerformed(evt);
+            }
+        });
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanelLog.setBackground(new java.awt.Color(255, 255, 255));
 
-        jTextArea1.setEditable(false);
-        jTextArea1.setColumns(20);
-        jTextArea1.setForeground(new java.awt.Color(168, 168, 168));
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        txtLog.setEditable(false);
+        txtLog.setColumns(20);
+        txtLog.setForeground(new java.awt.Color(168, 168, 168));
+        txtLog.setRows(5);
+        JScrollPane1.setViewportView(txtLog);
 
-        lblConsole1.setForeground(new java.awt.Color(136, 136, 136));
-        lblConsole1.setText("Estado:");
+        lblLog.setForeground(new java.awt.Color(136, 136, 136));
+        lblLog.setText("Estado:");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanelLogLayout = new javax.swing.GroupLayout(jPanelLog);
+        jPanelLog.setLayout(jPanelLogLayout);
+        jPanelLogLayout.setHorizontalGroup(
+            jPanelLogLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelLogLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(lblConsole1)
+                .addGroup(jPanelLogLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(JScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
+                    .addGroup(jPanelLogLayout.createSequentialGroup()
+                        .addComponent(lblLog)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        jPanelLogLayout.setVerticalGroup(
+            jPanelLogLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelLogLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lblConsole1)
+                .addComponent(lblLog)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE)
+                .addComponent(JScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         btnRead.setText("Leer Huella");
+        btnRead.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnReadActionPerformed(evt);
+            }
+        });
+
+        lblStatus.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lblStatus.setText("Intentos");
 
         javax.swing.GroupLayout jPanelBackgroundLayout = new javax.swing.GroupLayout(jPanelBackground);
         jPanelBackground.setLayout(jPanelBackgroundLayout);
@@ -123,15 +341,16 @@ public class Enrollment extends JApplet {
             jPanelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelBackgroundLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanelBackgroundLayout.createSequentialGroup()
                         .addComponent(picFingerprint, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jPanelConsole, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelBackgroundLayout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
+                            .addComponent(jPanelLog, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(jPanelBackgroundLayout.createSequentialGroup()
+                        .addComponent(lblStatus)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnRead)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnSave)
@@ -147,13 +366,14 @@ public class Enrollment extends JApplet {
                     .addGroup(jPanelBackgroundLayout.createSequentialGroup()
                         .addComponent(jPanelConsole, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jPanelLog, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(picFingerprint, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
                 .addGroup(jPanelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnCancel)
                     .addComponent(btnSave)
-                    .addComponent(btnRead))
+                    .addComponent(btnRead)
+                    .addComponent(lblStatus))
                 .addContainerGap())
         );
 
@@ -167,21 +387,34 @@ public class Enrollment extends JApplet {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
+
     }// </editor-fold>                        
 
+    private void btnReadActionPerformed(java.awt.event.ActionEvent evt) {                                        
+        //init();
+        //updateStatus();
+        //start();
+    }                                       
+
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        System.exit(0);
+    }                                         
+
     // Variables declaration - do not modify                     
+    private javax.swing.JScrollPane JScrollPane1;
     private javax.swing.JButton btnCancel;
     private javax.swing.JButton btnRead;
     private javax.swing.JButton btnSave;
     private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanelBackground;
     private javax.swing.JPanel jPanelConsole;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JPanel jPanelLog;
     private javax.swing.JLabel lblConsole;
-    private javax.swing.JLabel lblConsole1;
+    private javax.swing.JLabel lblLog;
+    private javax.swing.JLabel lblStatus;
     private javax.swing.JLabel picFingerprint;
     private javax.swing.JTextField txtConsole;
+    private javax.swing.JTextArea txtLog;
     // End of variables declaration                   
 }
+
